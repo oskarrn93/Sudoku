f5ac94925fabee1097a6e4cb87c3bcfa56eaff82
Oskar Rosén <oskarrn93@gmail.com>
2018-12-10 22:27:36 +0100

    added generateboard function which sets all cells to 0

--- Sudoku.java
+++ Sudoku.java
@@ -2,5 +2,24 @@
 class Sudoku {
    public static void main(String[] args) {
       System.out.println("hello world");
+
+      int[][] board = generateBoard();
    }
+
+   private static int[][] generateBoard() {
+      return generateBoard(9, 9);
+   }
+
+   private static int[][] generateBoard(int rows, int cols) {
+      int[][] board = new int[rows][cols];
+
+      for(int a = 0; a < rows; a++) {
+         for(int b = 0; b < cols; b++) {
+            board[a][b] = 0;
+         }
+      }
+
+      return board;
+   }
+
 }
