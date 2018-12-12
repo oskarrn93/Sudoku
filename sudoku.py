d1e98a1aca8c519f8181d0a495335f11d0796161
Oskar Rosén <oskarrn93@gmail.com>
2018-12-12 22:12:34 +0100

    lade till funktioner för att hämta ut rad och kolumn i matrisen
    lade även till funktion för att validera om ett visst värde existerar i rad eller kolumn

--- sudoku.py
+++ sudoku.py
@@ -3,7 +3,6 @@
 DEBUG = True
 #DEBUG = False
 
-
 def getGrid():
    #https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg/1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png
    grid = [
@@ -19,7 +18,47 @@ def getGrid():
    ] 
    return grid
 
+def getRow(grid, row):
+   result = list()
+
+   #hämta ut värdena från raden
+   for x in range(9):
+      result.append(grid[row][x])
+
+   if DEBUG: print("getRow", result)
+   return result
+
+def getColumn(grid, col):
+   result = list()
+
+   #hämta ut värdena från kolumnen
+   for x in range(9):
+      result.append(grid[x][col])
+
+   if DEBUG: print("getColumn", result)
+   return result
+
+def validateRow(grid, row, value):
+   rowValues = getRow(grid, row)
+   #kolla så att value inte matchar något värde som redan finns i samma rad
+   if(value in rowValues): 
+      return False
+   return True
+
+def validateColumn(grid, col, value):
+   colValues = getColumn(grid, col)
+   #kolla så att value inte matchar något värde som redan i samma kolumn
+   if(value in colValues): 
+      return False
+   return True
+
 if __name__ == "__main__":
 
    grid = getGrid()
    print("grid", grid)
+
+   print("validateRow", validateRow(grid, 0, 5)) #siffrorna är bara testfall
+   print("validateRow", validateRow(grid, 2, 5))
+   print("validateColumn", validateColumn(grid, 1, 3))
+   print("validateColumn", validateColumn(grid, 0, 3))
+
